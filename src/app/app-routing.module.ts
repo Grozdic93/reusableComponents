2bb3bb6d6073ea7bd410285084ad2e67fa7b3b10
Grozdic93 <dusgrozdic@gmail.com>
2024-06-26 11:55:47 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path:'details',
+    component: DetailsComponent
+  },
+ {
+  path:'products',
+  component: ProductsComponent
+ },
+ {
+  path: '**',
+  redirectTo: '/home'
+ }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LinkComponent } from './shared/link/link.component';
+import { ButtonComponent } from './shared/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    DetailsComponent,
+    HomeComponent,
+    NavbarComponent,
+    LinkComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
